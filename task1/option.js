f2aabd10af508482eb2a1ce05dc10936edc6789d
GlebKasyak <52315150+GlebKasyak@users.noreply.github.com>
2020-10-05 22:58:12 +0300

    fix: add parse option methods

--- task1/option.js
+++ task1/option.js
@@ -1,18 +1,37 @@
+const fs = require("fs");
+
+const { errorHandler } = require("./utils");
 
 module.exports = (program, action) => {
     return program
         .storeOptionsAsProperties(false)
         .passCommandToAction(false)
-        .requiredOption('-s, --shift <number>', 'a shift', value => parseInt(value))
-        .option('-i, --input <path>', 'an input file')
-        .option('-o, --output <path>', 'an output file')
-        .requiredOption('-a, --action <action>', 'an action encode/decode', value => {
-            let action = value.toLocaleLowerCase().trim();
-            if(action === "encode" || action === "decode") return value;
-
-            console.error("error: You entered an invalid action. Correct: 'encode' or 'decode'");
-            return process.exit(1);
-        })
+        .requiredOption('-s, --shift <number>', 'a shift', parseInteger)
+        .option('-i, --input <path>', 'an input file', parsePath)
+        .option('-o, --output <path>', 'an output file', parsePath)
+        .requiredOption('-a, --action <action>', 'an action encode/decode', parseAction)
         .action(action)
         .parse(process.argv)
+};
+
+function parseInteger(value) {
+    const int = parseInt(value)
+    if (isNaN(int)) errorHandler(`error: '${ value }' is invalid shift value`);
+
+    return int;
+};
+
+function parsePath(filePath){
+    if(!fs.existsSync(filePath)){
+        errorHandler(`error: incorrect this path: "${ filePath }", or write file does not exists`)
+    };
+
+    return filePath
+};
+
+function parseAction(value) {
+    let action = value.toLocaleLowerCase().trim();
+    if(action === "encode" || action === "decode") return value;
+
+    errorHandler("error: You entered an invalid action. Correct: 'encode' or 'decode'");
 };
